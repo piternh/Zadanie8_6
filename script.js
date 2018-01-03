ecc306df4c9ca5f718ea62f5a3ebf48bb36dbafb
Piotr Cybulski <piotrekkc2@gmail.com>
2018-01-03 14:42:57 +0100

    Add index and script files with code

---
+++ script.js
@@ -0,0 +1,12 @@
+var a = 2;
+var b = 3;
+var value = (a * a) - (2 * a * b) + (b * b);
+console.log(value);
+
+if (value > 0) {
+  console.log('Wynik dodatny');
+} else if (value < 0) {
+  console.log('Wynik ujemny');
+} else if (value = 0) {
+  console.log('Wynik jest równy zeru');
+}
